c266e220ba7cb216f858f1d179beb0a0f481bbf3
Keith Gregory <contact@kdgregory.com>
2021-07-31 08:19:08 -0400

    cloudwatch-log-cleanup: various updates
        * shuffle log groups before processing
        * add sleeps to avoid throttling
        * refactoring
        * adopt PEP-8 naming conventions

--- lambda/cloudwatch-log-cleanup/README.md
+++ lambda/cloudwatch-log-cleanup/README.md
@@ -1,21 +1,78 @@
-Lambda function to delete CloudWatch log streams that have exceeded their group's
+# CloudWatch Logs Cleanup
+
+A Lambda function to delete CloudWatch log streams that have exceeded their group's
 retention period. This is needed because CloudWatch will delete the messages but
 not the streams; in an environment that creates lots of streams (like Lambda), you
-will end up with lots of empty streams.
+will end up with lots of empty streams. While these may or may not affect the
+performance of CloudWatch Logs operations such as Insights, it's confusing to see
+them all in the Console.
 
-## Lambda Configuration
+
+## Deploying Manually
+
+Lambda Configuration
 
 * Runtime: Python 3.7
-* Default memory (128M) should be sufficient unless you have an extraordinary
-  number of groups/streams.
-* Timeout depends on the number of groups/streams, but 60 seconds should be
-  more than sufficient.
+* Default memory (128M) is sufficient unless you have a large number of groups
+  and/or streams. Increasing to 256M will improve performance slightly.
+* Timeout depends on the number of groups/streams, but 120 seconds should be
+  OK for most purposes.
 * Run outside of VPC; needs access to CloudWatch Logs endpoint.
 * Trigger via scheduled CloudWatch Event.
 
-## Permissions Required
+Permissions (in addition to basic Lambda permissions):
 
 * `logs:DeleteLogStream`
 * `logs:DescribeLogGroups`
 * `logs:DescribeLogStreams`
 * `logs:GetLogEvents`
+
+
+## Deploying with CloudFormation
+
+The file `cloudformation.yml` contains a CloudFormation template that creates
+the Lambda along with its log group, execution role, and event trigger.
+
+To run, you must first create a ZIPfile containing `lambda_function.py`, and
+then upload that ZIPfile to some place on S3. Finally, create a stack, providing
+the S3 bucket and key of the uploaded ZIPfile.
+
+
+## Usage Notes
+
+### Large numbers of log groups/streams
+
+If you have a large number of groups or streams, this Lambda might not be able to
+examine all of them before timing out. To work around this problem, it does two
+things:
+
+* First, itt uses an an environment variable, `MAX_LOG_GROUPS`, to limit the number
+  of log groups it will examine per invocation.
+
+  It not set, this value defaults to 1,000,000, which is the [documented
+  quota](https://docs.aws.amazon.com/AmazonCloudWatch/latest/logs/cloudwatch_limits_cwl.html)
+  for log groups per account and region. If you have anywhere near this number of
+  groups, then a Lambda is the wrong solution to the problem.
+
+  The CloudFormation script defaults this value to 60, with a Lambda timeout of 180
+  seconds. This ration is based on my experience; if you have a large number of
+  streams per group, you may need to either reduce the number of groups examined
+  or increase the Lambda timeout.
+
+* The second thing that the Lambda does is to shuffle log groups before processing
+  them. This ensures that, even if the Lambda does not run long enough to examine
+  all groups, multiple runs should cover everything.
+
+Another issue with large numbers of groups and streams is that the API calls are
+throttled at a relatively low rate: `DescribeLogGroups` and `DescribeLogStreams`
+default to 5 calls per second (these can be increased), while `GetLogEvents`
+is limited to 10 (which can't be increased). There's no documented quota for
+`DeleteLogStream`.
+
+To avoid throttling, which can impact other applications, this Lambda uses sleeps to
+rate-limit its calls. It's also resilient to throttling exceptions: any exceptions
+are trapped at the group level, so subsequent groups will continue processing.
+
+Finally, if you have a large number of groups/streams, you should schedule the
+Lambda to run more frequently than once a day. Start with every hour and see how
+long it takes to run.
--- lambda/cloudwatch-log-cleanup/lambda_function.py
+++ lambda/cloudwatch-log-cleanup/lambda_function.py
@@ -1,4 +1,4 @@
-# Copyright 2019 Keith D Gregory
+# Copyright 2019-2021 Keith D Gregory
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,37 +19,74 @@
 ################################################################################
 
 import boto3
+import os
+import random
 import time
-from datetime import datetime
+import sys
+
+from datetime import datetime, timezone
 from datetime import timedelta
 
-client = boto3.client('logs')
 
 def lambda_handler(event, context):
+    client = boto3.client('logs')
+    limit = int(os.environ.get('MAX_LOG_GROUPS', "1000000"))
+    for group in retrieve_log_groups(client, limit):
+        try:
+            process_log_group(client, group)
+        except:
+            print(f"exception while processing log group {group.get('logGroupName')}: {sys.exc_info()[1]}; skipping to next")
+
+
+def process_log_group(client, group):
+    group_name = group['logGroupName']
+    retention = group.get('retentionInDays')
+    print(f"examining {group_name}, retention period = {retention}")
+    if (retention == None):
+        return
+    retention_limit = datetime.now(tz=timezone.utc) - timedelta(days=retention)
+    for stream in retrieve_log_streams(client, group_name):
+        process_log_stream(client, group_name, stream, retention_limit)
+        time.sleep(0.1)
+
+
+def process_log_stream(client, group_name, stream, retention_limit):
+    stream_name = stream['logStreamName']
+    creation_time = datetime.fromtimestamp(stream['creationTime']/1000, timezone.utc)
+    if (creation_time > retention_limit):
+        return
+    events = client.get_log_events(logGroupName=group_name, logStreamName=stream_name, limit=10).get('events')
+    if events:
+        return
+    print(f"deleting {group_name} / {stream_name}: "
+                f"created {creation_time.isoformat(sep=' ', timespec='seconds')}, "
+                f"horizon {retention_limit.isoformat(sep=' ', timespec='seconds')}")
+    client.delete_log_stream(logGroupName=group_name, logStreamName=stream_name)
+
+
+def retrieve_log_groups(client, limit):
+    result = []
     paginator = client.get_paginator('describe_log_groups')
     itx = paginator.paginate()
     for page in itx:
         for group in page['logGroups']:
-            processLogGroup(group['logGroupName'], group.get('retentionInDays'))
+            result.append(group)
+        time.sleep(0.2)
+    random.shuffle(result)
+    if len(result) > limit:
+        print(f"retrieved {len(result)} log groups; limiting to {limit}")
+        return result[:limit]
+    else:
+        print(f"retrieved {len(result)} log groups")
+        return result
 
-def processLogGroup(groupName, retentionInDays):
-    if (retentionInDays == None):
-        return
-    retentionLimit = (datetime.utcnow() - timedelta(days=retentionInDays) - timedelta(days=1)).timestamp()
-    retentionLimitStr = time.asctime(time.gmtime(retentionLimit))
-    print(f"examining {groupName}, retention period = {retentionInDays}, retention limit = {retentionLimitStr}")
+
+def retrieve_log_streams(client, group_name):
+    result = []
     paginator = client.get_paginator('describe_log_streams')
-    itx = paginator.paginate(logGroupName=groupName)
+    itx = paginator.paginate(logGroupName=group_name)
     for page in itx:
         for stream in page['logStreams']:
-            processLogStream(groupName, stream['logStreamName'], stream['creationTime']/1000, retentionLimit)
-
-def processLogStream(groupName, streamName, creationTime, retentionLimit):
-    if (creationTime > retentionLimit):
-        return
-    events = client.get_log_events(logGroupName=groupName, logStreamName=streamName, limit=10).get('events')
-    if events:
-        return
-    creatonTimeStr = time.asctime(time.gmtime(creationTime))
-    print(f"deleting {groupName} / {streamName}: created {creatonTimeStr}")
-    client.delete_log_stream(logGroupName=groupName, logStreamName=streamName)
+            result.append(stream)
+        time.sleep(0.2)
+    return result
