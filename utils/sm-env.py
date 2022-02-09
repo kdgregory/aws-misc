a7692f500142b8ff3eae6e33e4705cd2a7360ad7
Keith Gregory <contact@kdgregory.com>
2022-02-09 07:39:27 -0500

    sm-env.py: dump module doc on error; allow invocation as sm-val.py

--- utils/sm-env.py
+++ utils/sm-env.py
@@ -14,60 +14,73 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 ################################################################################
-#
-# Retrieves a Secrets Manager secret and writes commands to set environment
-# variables with the values. Supports single-value secrets, or those containing
-# a stringified JSON object.
-#
-# Invocation:
-#
-#   rm-env.py SECRET_NAME (VARNAME | VARNAME=KEY ...)
-#
-# Where:
-#
-#   SECRET_NAME identifies the Secrets Manager secret. May be either the
-#               name of the secret or its ARN.
-#   VARNAME     is the desired name of an environment variable.
-#   KEY         identifies a value within a JSON secret.
-#
-#   There are two ways to invoke this function:
-#
-#    - If you provide a single variable name, by itself, then it is set
-#      to the entire secret value.
-#    - If you provide a series of VARNAME=KEY pairs, then the secret is
-#      assumed to contain a stringified JSON object. It is parsed, and
-#      each variable is set to the value of the specified key. If the
-#      key does not exist, this program emits an empty assignment.
-#
-# Example:
-#
-#   Note that this example wraps the invocation in $(), which cause Bash to
-#   execute the commands and update its environment.
-#
-#   $(sm-env.py DatabaseConnection PGHOST=QueueName QUEUE_URL=QueueUrl)
-#
-################################################################################
+
+""" 
+    Retrieves a Secrets Manager secret, and either returns a single value or
+    writes commands to set environment variables with the values. Supports
+    single-value secrets, and those containing a stringified JSON object.
+
+    Invocation:
+
+        sm-env.py SECRET_NAME (VARNAME | VARNAME=KEY ...)
+        sm-val.py SECRET_NAME [KEY]
+
+    Where:
+
+        SECRET_NAME identifies the Secrets Manager secret. May be either the
+                    name of the secret or its ARN.
+        VARNAME     is the desired name of an environment variable.
+        KEY         identifies a value within a JSON secret.
+
+    There are four ways to invoke this function:
+
+     - As sm-var.py, to output the entire value of the secret.
+     - As sm-var.py, to output a keyed value from a JSON-encoded secret.
+     - As sm-env.py, to output an environment variable assignment using
+       the entire value of the secret.
+     - As sm-env.py, to output one or more environment variable assignments
+       using keys from within JSON-encoded secrets.
+
+    Example: set multiple environment variables from contents of a JSON
+    secret (note that the $() construct executes the output):
+
+        $(sm-env.py DatabaseConnection PGHOST=QueueName QUEUE_URL=QueueUrl)
+    """
 
 import boto3
 import json
+import os
 import sys
 
 
-def retrieve_secret(secret_id):
-    """ Retrieves the secret's value.
-    """
-    client = boto3.client('secretsmanager')
-    return client.get_secret_value(SecretId=secret_id)['SecretString']
+def _check_usage(expected_arg_count=None):
+    if (expected_arg_count is None) or (len(sys.argv) < expected_arg_count):
+        print(__doc__, file=sys.stderr)
+        sys.exit(1)
 
 
 if __name__ == "__main__":
-    value = retrieve_secret(sys.argv[1])
-    if not "=" in sys.argv[2]:
-        print(f"export {sys.argv[2]}={value}")
+    _check_usage(2)
+    client = boto3.client('secretsmanager')
+    secret_id = sys.argv[1]
+    value = client.get_secret_value(SecretId=secret_id)['SecretString']
+
+    if os.path.basename(__file__) == 'sm-env.py':
+        _check_usage(3)
+        if not "=" in sys.argv[2]:
+            print(f"export {sys.argv[2]}={value}")
+        else:
+            lookup = json.loads(value)
+            for arg in sys.argv[2:]:
+                kv = arg.split('=')
+                val = lookup.get(kv[1])
+                if val:
+                    print(f"export {kv[0]}={val}")
+    elif os.path.basename(__file__) == 'sm-val.py':
+        if len(sys.argv) == 2:
+            print(value)
+        else:
+            lookup = json.loads(value)
+            print(lookup[sys.argv[2]])
     else:
-        lookup = json.loads(value)
-        for arg in sys.argv[2:]:
-            kv = arg.split('=')
-            val = lookup.get(kv[1])
-            if val:
-                print(f"export {kv[0]}={val}")
+        _check_usage() # force exit
