3bba9db98f367b45ed274650b294195cf056a24d
Keith Gregory <contact@kdgregory.com>
2022-02-14 07:50:12 -0500

    cf-env: convert header comment to module doc; print on invalid invocation

--- utils/cf-env.py
+++ utils/cf-env.py
@@ -1,50 +1,45 @@
 #!/usr/bin/env python3
 ################################################################################
 # Copyright 2019 Keith D Gregory
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 ################################################################################
-#
-# Retrieves parameters and outputs from a named CloudFormation stack and writes
-# environment variable export commands for specified values.
-#
-# Invocation:
-#
-#   cf-env.py STACK_NAME (VARNAME=ParmName | VARNAME=OutputName) ...
-#
-# Where:
-#
-#   STACK_NAME  identifies a CloudFormation stack accessible to the caller.
-#   VARNAME     is the desired name of an environment variable.
-#   ParmName    is the logical name of a stack parameter.
-#   OutputName  is the logical name (not export name) or a stack output.
-#
-#   If there is no parameter/output with the specified name, it is silently
-#   ignored.
-#
-# Example:
-#
-#   Note that this example wraps the invocation in $(), which cause Bash to
-#   execute the commands and update its environment.
-#
-#   $(cf-env.py ExampleSQS QUEUE_NAME=QueueName QUEUE_URL=QueueUrl)
-#
-# Programmatic use:
-# 
-#   retrieve_stack_values() - Retrieves the parameters and outputs from a named
-#                             stack and returns them as a dictionary.
-#
-################################################################################
+
+""" 
+Retrieves parameters and outputs from a named CloudFormation stack and writes
+environment variable export commands for specified values.
+
+Invocation:
+
+    cf-env.py STACK_NAME (VARNAME=ParmName | VARNAME=OutputName) ...
+
+Where:
+
+    STACK_NAME  identifies a CloudFormation stack accessible to the caller.
+    VARNAME     is the desired name of an environment variable.
+    ParmName    is the logical name of a stack parameter.
+    OutputName  is the logical name (not export name) or a stack output.
+
+    If there is no parameter/output with the specified name, it is silently
+    ignored.
+
+Example:
+
+    Note that this example wraps the invocation in $(), which cause Bash to
+    execute the commands and update its environment.
+
+    $(cf-env.py ExampleSQS QUEUE_NAME=QueueName QUEUE_URL=QueueUrl)
+"""
 
 import boto3
 import sys
@@ -69,6 +64,9 @@ def retrieve_stack_values(stack_name):
 
 
 if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print(__doc__, file=sys.stderr)
+        sys.exit(1)
     lookup = retrieve_stack_values(sys.argv[1])
     for arg in sys.argv[2:]:
         kv = arg.split('=')
