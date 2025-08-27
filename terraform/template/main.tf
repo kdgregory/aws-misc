5c9683ab92a86c047c8608a0cc151d1922247ec0
Keith Gregory <contact@kdgregory.com>
2025-08-27 08:29:16 -0400

    update Terraform template with current AWS provider version, replace deprecated attribute

--- terraform/template/main.tf
+++ terraform/template/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.1.0"
+      version = ">= 6.0"
     }
   }
 }
@@ -19,5 +19,5 @@ data "aws_region" "current" {}
 
 locals {
   aws_account_id  = data.aws_caller_identity.current.account_id
-  aws_region      = data.aws_region.current.name
+  aws_region      = data.aws_region.current.region
 }
